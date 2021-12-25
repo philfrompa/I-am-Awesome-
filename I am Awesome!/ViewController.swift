2f420a5cb92b7f97c2c3ab43bfeb748be97f3b6c
Phil Long <plong@ptd.net>
2021-12-25 18:03:29 -0500

    Initial Commit

---
+++ I am Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Awesome!
+//
+//  Created by Philip Long on 12/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I am Awesome!UITests/I_am_Awesome_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_am_Awesome_UITestsLaunchTests.swift
+//  I am Awesome!UITests
+//
+//  Created by Philip Long on 12/24/21.
+//
+
+import XCTest
+
+class I_am_Awesome_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
